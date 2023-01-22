f3575c8c3aab9738c6699642cceb37105647e2b1
Alexander <84810149+chipolinkin@users.noreply.github.com>
2023-01-22 16:21:34 +0400

    Update test_product_page.py

--- test_product_page.py
+++ test_product_page.py
@@ -1,7 +1,6 @@
 from selenium.webdriver.common.by import By
 from .pages.product_page import ProductPage
 from .pages.base_page import BasePage
-import time
 import pytest
 
 @pytest.mark.parametrize('link', ["http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0",
@@ -17,11 +16,8 @@ import pytest
 def test_add_product_to_basket(browser, link):
     page = ProductPage(browser, link)
     page.open()
-    time.sleep(5)
     page.add_product_to_basket()
-    time.sleep(2)
     page.solve_quiz_and_get_code()
-    time.sleep(2)
     page.right_name()
     page.right_price()
 
